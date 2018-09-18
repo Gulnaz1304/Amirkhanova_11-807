df2ce487637931a5cce999da1a87cf91e326180e
Gulnaz Amirkhanova <mrrrrrrrrrrrrr1@icloud.com>
2018-09-18 04:54:09 +0300

    added

---
+++ Homework03/Local.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Local {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("введите длину массива");
+        int dl = n.nextInt();
+        int arr[] = new int[dl];
+        System.out.println("введите элементы массива");
+        for (int i = 0; i < dl; i++) {
+            arr[i] = n.nextInt();
+        }
+        int k = 0;
+        for (int i = 1; i < dl-1; i++) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i+1]) {
+
+                k = k + 1;
+            }
+
+        }
+        System.out.println(k);
+
+    }
+}
+
---
+++ Homework03/Max.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Max {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("введите количество элементов в массиве");
+        int raz = n.nextInt();
+        int arr[] = new int[raz];
+        System.out.println("ведите элементы массива");
+        for (int i = 0; i < raz; i++) {
+            arr[i] = n.nextInt();
+        }
+        int max1 = 0;
+        for (int i = 0; i < raz; i++) {
+            if (Math.abs(arr[i]) > Math.abs(max1)) {
+                max1 = arr[i];
+
+            }
+        }
+        System.out.println(max1);
+
+    }
+}
